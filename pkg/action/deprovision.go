8d383efbbfffff4b040074b882a369460ae411f5
Ryan Hallisey <rhallise@redhat.com>
2017-09-08 17:32:28 -0400

    Remove namespace from deprovision
    We will use the local namespace.

--- pkg/action/deprovision.go
+++ pkg/action/deprovision.go
@@ -6,8 +6,8 @@ import (
 
 func Deprovision(repo string, cmd string) error {
 	resource := resourceName(repo)
-	fmt.Printf("Running: %s delete ServiceInstance %s -n default\n", cmd, resource)
-	output, err := RunCommand(cmd, "delete", "ServiceInstance", resource, "-n", "default")
+	fmt.Printf("Running: %s delete ServiceInstance %s\n", cmd, resource)
+	output, err := RunCommand(cmd, "delete", "ServiceInstance", resource)
 	if err != nil {
 		return err
 	}
