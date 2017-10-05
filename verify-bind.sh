a23da9d5b5a8545a9afed62e1dbad20008e9c18e
Ryan Hallisey <rhallise@redhat.com>
2017-10-05 14:48:07 -0400

    Expect the downloaded scripts to not be executable

--- verify-bind.sh
+++ verify-bind.sh
@@ -10,7 +10,7 @@ sleep 30
 RETRIES=10
 for x in $(seq $RETRIES); do
     oc delete pods $(oc get pods -o name -l app="${bindApp}" | head -1 | cut -f 2 -d '/') --force --grace-period=10
-    .${TEMPLATE_DIR}/wait-for-resource.sh create pod mediawiki
+    bash ${TEMPLATE_DIR}/wait-for-resource.sh create pod mediawiki
 
     # Filter for 'podpreset.admission.kubernetes.io' in the pod
     preset_test=$(oc get pods $(oc get pods | grep mediawiki | grep Running | awk $'{ print $1 }') -o yaml | grep podpreset | awk $'{ print $1}' | cut -f 1 -d '/')
