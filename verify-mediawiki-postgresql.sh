14b2edad6791a86a266a9d44a1564b7c390dce18
Ryan Hallisey <rhallise@redhat.com>
2017-10-31 12:23:37 -0400

    Exit 1 when the verify-mediawiki-postgres check fails

--- verify-mediawiki-postgresql.sh
+++ verify-mediawiki-postgresql.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-ROUTE=$(oc get route -n default | grep mediawiki | cut -f 4 -d ' ')/index.php/Main_Page
+ROUTE=$(oc get route | grep mediawiki | cut -f 4 -d ' ')/index.php/Main_Page
 BIND_CHECK=$(curl ${ROUTE}| grep "div class" | cut -f 2 -d "'")
 echo "Running: curl ${ROUTE}| grep \"div class\" | cut -f 2 -d \"'\""
 
 if [ "${BIND_CHECK}" = "" ] || [ "${BIND_CHECK}" = "error" ]; then
         echo "Failed to gather data from ${ROUTE}"
+	exit 1
 else
     echo "SUCCESS"
     echo "You can double check by opening http://${ROUTE} in your browser"
