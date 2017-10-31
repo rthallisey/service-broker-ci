e0dfb5ba9154c2de216dd5944d86f325004d8ebb
Ryan Hallisey <rhallise@redhat.com>
2017-10-31 16:22:04 -0400

    Add retry logic to verify bind

--- verify-mediawiki-postgresql.sh
+++ verify-mediawiki-postgresql.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 ROUTE=$(oc get route | grep mediawiki | cut -f 4 -d ' ')/index.php/Main_Page
-BIND_CHECK=$(curl ${ROUTE}| grep "div class" | cut -f 2 -d "'")
 echo "Running: curl ${ROUTE}| grep \"div class\" | cut -f 2 -d \"'\""
+RETRIES=60
 
-if [ "${BIND_CHECK}" = "" ] || [ "${BIND_CHECK}" = "error" ]; then
+for r in $(seq $RETRIES); do
+    BIND_CHECK=$(curl ${ROUTE}| grep "div class" | cut -f 2 -d "'")
+    if [ "${BIND_CHECK}" = "" ] || [ "${BIND_CHECK}" = "error" ]; then
         echo "Failed to gather data from ${ROUTE}"
-	exit 1
-else
-    echo "SUCCESS"
-    echo "You can double check by opening http://${ROUTE} in your browser"
+    else
+	echo "SUCCESS"
+	echo "You can double check by opening http://${ROUTE} in your browser"
+    fi
+    sleep 2
+done
+
+if [ "${r}" == "${RETRIES}" ]; then
+    echo "Error: Timeout waiting for verification"
+    exit 1
 fi
