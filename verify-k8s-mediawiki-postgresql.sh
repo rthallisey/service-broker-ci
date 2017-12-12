5fd1cba7800ce3d0b2e5e09a42af34574fecff1b
Ryan Hallisey <rhallise@redhat.com>
2017-12-12 11:38:24 -0500

    kubernetes ci gate

--- verify-k8s-mediawiki-postgresql.sh
+++ verify-k8s-mediawiki-postgresql.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-ENDPOINT=$(kubectl get endpoints | grep mediawiki | awk '{ print $2 }')/index.php/Main_Page
-echo "Running: curl ${ENDPOINT} | grep \"div class\" | cut -f 2 -d \"'\""
 RETRIES=60
 
 for r in $(seq $RETRIES); do
+    ENDPOINT=$(kubectl get endpoints | grep mediawiki | awk '{ print $2 }')/index.php/Main_Page
+    echo "Running: curl ${ENDPOINT} | grep \"div class\" | cut -f 2 -d \"'\""
+
     BIND_CHECK=$(curl ${ENDPOINT} | grep "div class" | cut -f 2 -d "'")
     if [ "${BIND_CHECK}" = "" ] || [ "${BIND_CHECK}" = "error" ]; then
         echo "Failed to gather data from ${ENDPOINT}"
     else
-	echo "SUCCESS"
-	echo "You can double check by opening http://${ENDPOINT} in your browser"
-	break
+        echo "SUCCESS"
+        echo "You can double check by opening http://${ENDPOINT} in your browser"
+        break
     fi
     sleep 2
 done
